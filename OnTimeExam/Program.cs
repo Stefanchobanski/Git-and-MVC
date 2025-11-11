185b4c9f24532c61ddc05ca2853e83fdbd7fcdac
StefanChobanski <stefanchobanski@gmail.com>
2025-11-11 19:36:07 +0200

    add files

---
+++ OnTimeExam/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OnTimeExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
